10f0848bde33857b37862e3501a08ebe686862f1
richardbryan <richard.bryan1680@gmail.com>
2022-08-15 15:22:21 +0800

    setup mlflow metrics logging (jalik's code)

--- Richard/train.py
+++ Richard/train.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[113]:
-
-
+import mlflow
 import pandas as pd
 import os
 from sklearn.preprocessing import LabelEncoder
 from sklearn.naive_bayes import *
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+from joblib import dump
 
-# In[110]:
-
+mlflow.set_tracking_uri('https://community.mlflow.deploif.ai')
+mlflow.set_experiment("Deploifai/Lichess/LichessRichard")
 
 train = pd.read_csv('dataset/train.csv')
 test = pd.read_csv('dataset/test.csv')
@@ -24,20 +21,12 @@ drop_cols = ['Unnamed: 0', 'winner', 'opening_eco']
 train.drop(columns=drop_cols, inplace=True)
 test.drop(columns=drop_cols, inplace=True)
 
-
-# In[105]:
-
-
 encoder = LabelEncoder()
 train_winner = encoder.fit_transform(train_winner)
 test_winner = encoder.fit_transform(test_winner)
 
 # train['opening_eco'] = encoder.fit_transform(train['opening_eco'])
 # test['opening_eco'] = encoder.fit_transform(test['opening_eco'])
-
-
-# In[111]:
-
 
 models = [BernoulliNB(), ComplementNB(), GaussianNB(), MultinomialNB(), KNeighborsClassifier()]
 results = []
@@ -46,11 +35,13 @@ for model in models:
     predict = model.predict(test)
     accuracy = accuracy_score(predict, test_winner)
     results.append(accuracy)
-print(results)
 
+best_accuracy = max(results)
+best_model = models[results.index(best_accuracy)]
 
-# In[ ]:
+dirname = os.path.dirname(os.path.realpath(__file__))
+print(dirname)
+dump(best_model, os.path.join(dirname, "artifacts", "model"))
 
-
-
-
+with mlflow.start_run() as run:
+    mlflow.log_metric("accuracy", best_accuracy)
