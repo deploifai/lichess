ed891c84d0597986536d39327c528fbbc5d43b7c
richardbryan <richard.bryan1680@gmail.com>
2022-08-17 16:18:09 +0800

    bug fix

--- Richard/train.py
+++ Richard/train.py
@@ -8,6 +8,11 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from joblib import dump
 
+# only if running an experiment outside deploifai (this must come before setting mlflow tracking uri and experiment)
+# os.environ["MLFLOW_TRACKING_USERNAME"] = "Deploifai/Lichess/LichessRichard"
+# os.environ["MLFLOW_TRACKING_PASSWORD"] = "drat-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNsNnE5b20wczM3MzE2M3MzZXE1NDFjMmNuIiwiaWF0IjoxNjYwNzI0MDgyLCJleHAiOjE2OTIyODE2ODJ9.13eh6Pvne8tvCyPsxTFLUhNDxqzAOwzknGu2rDlRhVE"
+
+# setup mlflow for this experiment
 mlflow.set_tracking_uri('https://community.mlflow.deploif.ai')
 mlflow.set_experiment("Deploifai/Lichess/LichessRichard")
 
@@ -25,9 +30,6 @@ encoder = LabelEncoder()
 train_winner = encoder.fit_transform(train_winner)
 test_winner = encoder.fit_transform(test_winner)
 
-train['opening_eco'] = encoder.fit_transform(train['opening_eco'])
-test['opening_eco'] = encoder.fit_transform(test['opening_eco'])
-
 models = [BernoulliNB(), ComplementNB(), GaussianNB(), MultinomialNB(), KNeighborsClassifier()]
 results = []
 for model in models:
@@ -37,10 +39,10 @@ for model in models:
     results.append(accuracy)
 
 best_accuracy = max(results)
+print(best_accuracy)
 best_model = models[results.index(best_accuracy)]
 
 dirname = os.path.dirname(os.path.realpath(__file__))
-print(dirname)
 dump(best_model, os.path.join(dirname, "artifacts", "model"))
 
 with mlflow.start_run() as run:
